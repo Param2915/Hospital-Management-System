e4d280b332bf068403391ad92ad0ddee41c72b40
Param2915 <123792929+Param2915@users.noreply.github.com>
2024-11-17 17:27:43 +0530

    17/11/2024

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import {catchAsyncError} from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/errorHandler.js";
 import {User} from "../models/userSchema.js";
 import {generateToken} from "../utils/jwtMiddleware.js";
+import cloudinary from "cloudinary";
 
 export const patientRegister = catchAsyncError(async(req,res,next)=>{
     const {firstName, lastName, email, phone, password, gender, age, role} = req.body;
@@ -85,5 +86,45 @@ export const patientLogOut = catchAsyncError(async(req,res,next)=>{
 })
 
 
+export const addNewDoctor = catchAsyncError(async(req,res,next)=>{
 
+    if(!req.files || Object.keys(req.files).length === 0){
+        return next(new ErrorHandler("Doctor Avatar Required"),400)
+    }
+    const { docAvatar } = req.files;
+
+    const allowedFormats = ["/image/png","/image/jpeg","/image/webp"];
+    if(!allowedFormats) return next(new ErrorHandler("File format not supported"),400);
+
+    const { firstName,lastName,age,gender,email,password,phone,doctorDepart } = req.body;
+
+    if(!firstName || !lastName || !age || !gender || !email || !password || !phone || !doctorDepart) {
+        return next(new ErrorHandler("Please provide all the details",400))
+    }
+
+    let user = await User.findOne({email});
+
+    if(user){
+        return next(new ErrorHandler(`${user.role} already registered with this email! `,400))
+    }
+
+    const cloudinaryResponse = await cloudinary.uploader.upload(docAvatar.tempFilePath)
+    if(!cloudinaryResponse || cloudinaryResponse.error){
+        console.error("Cloudinary Error: ",cloudinaryResponse.error || "Unknown Cloudinary Error")
+    }
+
+    const doctor = await User.create({firstName, lastName, email, phone, password, gender, age, doctorDepart,role:"Doctor",
+        docAvatar:{
+            public_id: cloudinaryResponse.public_id,
+            url:cloudinaryResponse.secure_url
+        }});
+
+    res.status(200).json({
+        success: true,
+        message: "New Doctor Registered",
+        doctor
+    })
+
+
+})
 // module.exports = { patientRegister, patientLogin };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { patientLogin, patientRegister,adminRegister, adminLogOut, patientLogOut } from "../controllers/userController.js";
+import { patientLogin, patientRegister,adminRegister, adminLogOut, patientLogOut, addNewDoctor } from "../controllers/userController.js";
 import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/jwtAuthMiddleware.js";
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.post("/login", patientLogin)
 router.post("/admin/register", isAdminAuthenticated,adminRegister)
 router.get("/admin/logout", isAdminAuthenticated,adminLogOut)
 router.get("/patient/logout",isPatientAuthenticated,patientLogOut)
+router.post("/doctor/register",isAdminAuthenticated,addNewDoctor)
 
 
 export default router;
